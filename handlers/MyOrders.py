70a753ce009779fb32a8cea5929de03a2ef6360c
apple <apple@appledeMacBook-Pro.local>
2018-12-17 17:32:28 +0800

    提交一下代码
    更新一下接口。

--- handlers/MyOrders.py
+++ handlers/MyOrders.py
@@ -1,3 +1,4 @@
+#coding:utf8
 import pymysql
 import json
 import logging
@@ -10,7 +11,8 @@ class OrdersHandlers(BaseHandler):
 
         # self.write('{"errno":1}')
         cur = self.db.cursor(cursor=pymysql.cursors.DictCursor)
-        sql = "select O.order_id ,O.order_date,O.price,O.booking_num,O.price,H.hotel_name,H.hotel_address,U.nick_name,U.phone_number from y_order_record_3 O left join y_hotel_info_8 H on O.hotel_Id=H.hotel_Id left join y_user_info_1 U on H.user_id=U.user_id    order by O.order_id DESC limit 0,10"
+        logging.error("这个接口执行了")
+        sql = "select O.order_number ,O.order_date,O.price,O.booking_num,O.unit_price,O.pay_type,O.booking_checkin_date,O.booking_leave_date,O.order_type,O.order_state,H.hotel_name,H.hotel_address, U.nick_name,U.phone_number from y_order_record_3 O left join y_hotel_info_8 H on O.hotel_Id=H.hotel_Id left join y_user_info_1 U on H.user_id=U.user_id    order by O.order_id DESC limit 0,10"
         try:
 
             ret =cur.execute(sql)
@@ -26,8 +28,18 @@ class OrdersHandlers(BaseHandler):
                 if l["order_date"]:
 
 
-                   l["order_date"] = l["order_date"].strftime("%Y-%m-%d")
+                   l["order_date"] = l["order_date"].strftime("%Y-%m-%d %H:%M:%S")
+                if l["booking_checkin_date"]:
+
+
+                   l["booking_checkin_date"] = l["booking_checkin_date"].strftime("%Y-%m-%d")
+                if l["booking_leave_date"]:
+
+
+                   l["booking_leave_date"] = l["booking_leave_date"].strftime("%Y-%m-%d")
+
                 a.append(l)
+
 
         logging.error(dict(data = a))
 
